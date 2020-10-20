b60ab2068d42ac7c719968aa16f9e9dab33b6011
Irina Selivanova <irina.selivanova1106@gmail.com>
2020-10-20 19:04:01 +0300

    feat: add check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsCouple = [],
+        tempString,
+        processString = true;
+
+    if (str.length % 2 !==0) {
+        return false;
+    }
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        bracketsCouple.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+
+    while (processString) {
+        tempString = str;
+        for (let j = 0; j <= bracketsCouple.length; j++) {
+            str = str.replace(bracketsCouple[j], '');
+        }
+
+        if (tempString === str || str.length === 0) {
+            processString = false;
+        }
+    }
+
+    return !str.length;
 }
